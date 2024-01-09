bed7b3f0a4896450b3925277e8925f956f1b60c8
MomsDeveloper <georgiy.khabner@mail.ru>
2024-01-09 09:16:52 +0300

    fix validation area for negative value

--- lab3/src/main/java/momsDeveloper/itmoweb/utils/AreaValidator.java
+++ lab3/src/main/java/momsDeveloper/itmoweb/utils/AreaValidator.java
@@ -2,19 +2,28 @@ package momsDeveloper.itmoweb.utils;
 
 public class AreaValidator {
     public static boolean checkArea(double x, double y, double r) {
-        return (checkSector(x, y, r) || checkTriangle(x, y, r) || checkSquare(x, y, r));        
+        return (checkSector(x, y, r) || checkTriangle(x, y, r) || checkSquare(x, y, r));
     }
 
     public static boolean checkSector(double x, double y, double r) {
-        return ((x * x + y * y) <= r * r && x >= 0 && y <= 0);
+        r = r / 2;
+        if (r <= 0) {
+            return ((x * x + y * y) <= r * r && x >= 0 && y >= 0);
+        } else
+            return ((x * x + y * y) <= r * r && x <= 0 && y <= 0);
     }
 
     public static boolean checkTriangle(double x, double y, double r) {
-        r = r / 2;
-        return ((-1) * x + y <= r && x <= 0 && y >= 0);
+        if (r <= 0) {
+            return ((-1)*x + y <= (-1)*r && x <= 0 && y >= 0);
+        } else
+            return (x + (-1) * y <= r && x >= 0 && y <= 0);
     }
 
     public static boolean checkSquare(double x, double y, double r) {
-        return (x >= 0 && y >= 0 && x <= r/2 && y <= r);
+        if (r <= 0) {
+            return (x >= 0 && y <= 0 && x <= (-1)*r / 2 && (-1) * y <= (-1)*r);
+        } else
+            return (x <= 0 && y >= 0 && (-1) * x <= r / 2 && y <= r);
     }
 }
